55bd53bb0edbf17c857d5c6e3df10a138636e82a
Anshu Ranjan <ranshu1601@gmail.com>
2022-08-19 15:00:45 +0530

    Updates

---
+++ precache-manifest.94829c4eb63b175777af6b62bb75b723.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a3e00210a259cba47b",
+    "url": "/React_Based_Portfolio/static/css/main.11985ad4.chunk.css"
+  },
+  {
+    "revision": "83a3e00210a259cba47b",
+    "url": "/React_Based_Portfolio/static/js/main.83a3e002.chunk.js"
+  },
+  {
+    "revision": "5697770ea0c8b19522fc",
+    "url": "/React_Based_Portfolio/static/css/1.0f546280.chunk.css"
+  },
+  {
+    "revision": "5697770ea0c8b19522fc",
+    "url": "/React_Based_Portfolio/static/js/1.5697770e.chunk.js"
+  },
+  {
+    "revision": "0366ca6df08a3d087f95",
+    "url": "/React_Based_Portfolio/static/js/runtime~main.0366ca6d.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/React_Based_Portfolio/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "b1e81d847e8ad09ad1f8624e768752f2",
+    "url": "/React_Based_Portfolio/static/media/me.b1e81d84.png"
+  },
+  {
+    "revision": "b4890ab7e474a1d2a33bbc0f85c8fa83",
+    "url": "/React_Based_Portfolio/static/media/me-about.b4890ab7.jpg"
+  },
+  {
+    "revision": "e37bce5e5fb807ef56ee1862f6d16834",
+    "url": "/React_Based_Portfolio/static/media/portfolio1.e37bce5e.jpg"
+  },
+  {
+    "revision": "4a6488875dfdda2055563c82b8928fbc",
+    "url": "/React_Based_Portfolio/static/media/portfolio2.4a648887.jpg"
+  },
+  {
+    "revision": "f9ce28df42b35ef85a1bc1527c3ab921",
+    "url": "/React_Based_Portfolio/static/media/portfolio3.f9ce28df.jpg"
+  },
+  {
+    "revision": "29e4c53ae7f8ec1a9a40b71845867747",
+    "url": "/React_Based_Portfolio/static/media/portfolio4.29e4c53a.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/React_Based_Portfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/React_Based_Portfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "e7b6ed88a61264dd8493d42d31bb3a13",
+    "url": "/React_Based_Portfolio/index.html"
+  }
+];
